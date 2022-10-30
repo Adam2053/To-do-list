15760e423c9aa2303b5381abcf534872164e1628
arpit marathe <arpitmarathe2002@gmail.com>
2022-10-30 22:33:50 +0530

    final commit

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.post('/groceries', (req, res)=>{
 
 
 // Server
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT, function(req, res){
     console.log("Server is running on port 3000");
 })
 
